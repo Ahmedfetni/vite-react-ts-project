a498f88562bcc238420f832b3c0c1b851a7d00f4
Ahmed Fetni <ahmedfetni277@gmail.com>
2023-08-11 11:42:00 +0100

    just a component for a heading and for sub heading in ts +react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Heading from "./components/Heading"
+import { Section } from "./components/Section"
+
+function App(){
+return( <>
+    <Heading title={"Hello"}/>
+    <Section>This is my <section></section></Section>
+  </>)
+}
+
+export default App
