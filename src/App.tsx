c7c4886f95ee2f0f3076469587cc7bdde3ce6b53
Ahmed Fetni <ahmedfetni277@gmail.com>
2023-08-11 11:53:12 +0100

    adding a counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Heading from "./components/Heading"
 import { Section } from "./components/Section"
+import Counter from "./components/Counter"
 
 function App(){
 return( <>
     <Heading title={"Hello"}/>
     <Section>This is my <section></section></Section>
+    <Counter/>
   </>)
 }
 
