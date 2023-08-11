8198d54ef32f0b2dbe8c4e761425e8a1b93f72e4
Ahmed Fetni <ahmedfetni277@gmail.com>
2023-08-11 15:32:31 +0100

    using react hook like use effect and usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from "react"
 import Heading from "./components/Heading"
-import { Section } from "./components/Section"
+import { Section } from "./components/Section";
 import Counter from "./components/Counter"
+import { useState, useEffect } from "react";
+ interface User {
+  id: number;
+  username: string
+ }
+
 
 function App(){
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [users, setUsers] = useState<User[]|null>(null)
+
+
+  useEffect(()=>{
+    console.log("mounting");
+    console.log("Users",users);
+
+    return ()=>console.log("unmounting ")
+  },[users])
+
+
 return( <>
     <Heading title={"Hello"}/>
     <Section>This is my <section></section></Section>
